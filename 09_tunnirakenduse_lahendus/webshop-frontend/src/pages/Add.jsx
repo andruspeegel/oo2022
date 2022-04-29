44222ea70035f01cd145e19eeb959d2f017b3fc3
andruspeegel <andrusp@tlu.ee>
2022-04-29 15:34:48 +0300

    29/04 tund

--- 09_tunnirakenduse_lahendus/webshop-frontend/src/pages/Add.jsx
+++ 09_tunnirakenduse_lahendus/webshop-frontend/src/pages/Add.jsx
@@ -1,6 +1,29 @@
+import { useRef } from "react";
+
 
 function Add() {
-    return(<div>Add</div>)
+    const nameRef = useRef();
+    const priceRef = useRef();
+    function addProduct() {
+        fetch("http://localhost:8080/products", {
+            method: "POST",
+            body: JSON.stringify({
+                name: nameRef.current.value, 
+                price: priceRef.current.value
+            }),
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+    }
+
+    return(<div>
+        <label htmlFor="">NIMI</label>
+        <input ref={nameRef} type="text" />
+        <label htmlFor="">HIND</label>
+        <input ref={priceRef} type="price" />
+        <button onClick={() => addProduct()}>Add</button>
+    </div>)
 }
 
 export default Add;
